2ff99fe6b225873d10330d2902c27d16615eabc5
Watykan2137 <90694327+Watykan2137@users.noreply.github.com>
2021-11-03 20:11:55 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.DrawDistance = 100.0
+Config.MarkerSize   = {x = 1.5, y = 1.5, z = 1.0}
+Config.MarkerColor  = {r = 102, g = 102, b = 204}
+Config.MarkerType   = 21
+
+Config.SimCoords = {
+    {x = -1082.33, y = -247.64, z = 37.76}
+}
